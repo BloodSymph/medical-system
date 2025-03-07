8dbd3ebeff1b8c7770c5a8f7bf0c14f84f878820
BloodSymph <rubakharoma@gmail.com>
2025-03-07 18:50:33 +0200

    Med Personal Service 2!

---
+++ med-personal/src/main/java/com/med/personal/repository/MedPersonalRepository.java
@@ -0,0 +1,14 @@
+package com.med.personal.repository;
+
+import com.med.personal.entity.MedPersonalEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MedPersonalRepository extends JpaRepository<MedPersonalEntity, Long> {
+
+    Optional<MedPersonalEntity> findByUsernameIgnoreCase(String username);
+
+}
--- med-personal/src/main/java/com/med/personal/service/implementation/MedPersonalServiceImplementation.java
+++ med-personal/src/main/java/com/med/personal/service/implementation/MedPersonalServiceImplementation.java
@@ -2,27 +2,62 @@ package com.med.personal.service.implementation;
 
 import com.med.personal.dto.MedPersonalRequest;
 import com.med.personal.dto.MedPersonalResponse;
+import com.med.personal.entity.MedPersonalEntity;
+import com.med.personal.excepton.errors.MedPersonalProfileNotFoundException;
+import com.med.personal.repository.MedPersonalRepository;
 import com.med.personal.service.MedPersonalService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.med.personal.mapper.MedPersonalMapper.mapMedPersonalRequestToEntity;
+import static com.med.personal.mapper.MedPersonalMapper.mapMedPersonalResponse;
+import static com.med.personal.util.GetUserFromCurrentAuthSession.getSessionUser;
+
 @Service
+@RequiredArgsConstructor
 public class MedPersonalServiceImplementation implements MedPersonalService {
+
+    private final MedPersonalRepository medPersonalRepository;
+
 
     @Override
     public MedPersonalResponse getProfile() {
-        return null;
+        MedPersonalEntity medPersonalEntity = medPersonalRepository
+                .findByUsernameIgnoreCase(getSessionUser())
+                .orElseThrow(
+                        () -> new MedPersonalProfileNotFoundException(
+                                "Cannot find profile for username: " + getSessionUser() + "!"
+                        )
+                );
+        return mapMedPersonalResponse(medPersonalEntity);
     }
 
     @Override
-    @Transactional
     public MedPersonalResponse createProfile(MedPersonalRequest medPersonalRequest) {
-        return null;
+        MedPersonalEntity medPersonalEntity = mapMedPersonalRequestToEntity(medPersonalRequest);
+        medPersonalEntity.setUsername(getSessionUser());
+        medPersonalRepository.save(medPersonalEntity);
+        return mapMedPersonalResponse(medPersonalEntity);
     }
 
     @Override
     @Transactional
     public MedPersonalResponse updateProfile(MedPersonalRequest medPersonalRequest) {
+        MedPersonalEntity medPersonalEntity = medPersonalRepository
+                .findByUsernameIgnoreCase(getSessionUser())
+                .orElseThrow(
+                        () -> new MedPersonalProfileNotFoundException(
+                                "Cannot find profile for username: " + getSessionUser() + "!"
+                        )
+                );
+        if(!medPersonalEntity.getVersion().equals(medPersonalRequest.getVersion())) {
+            throw new MedPersonalProfileNotFoundException(
+                    "Med personal profile entity version not valid!"
+            );
+        }
+
         return null;
     }
 
