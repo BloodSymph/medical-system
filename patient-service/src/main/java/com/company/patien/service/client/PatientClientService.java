4a0e69f977c7b6733f856125238f9bb578569163
BloodSymph <rubakharoma@gmail.com>
2025-04-23 14:17:11 +0300

    Patient service v4!

---
+++ patient-service/src/main/java/com/company/patien/service/client/PatientClientService.java
@@ -0,0 +1,10 @@
+package com.company.patien.service.client;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface PatientClientService {
+
+
+
+}
---
+++ patient-service/src/main/java/com/company/patien/service/client/implementation/PatientClientServiceImpl.java
@@ -0,0 +1,11 @@
+package com.company.patien.service.client.implementation;
+
+import com.company.patien.service.client.PatientClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PatientClientServiceImpl implements PatientClientService {
+
+}
