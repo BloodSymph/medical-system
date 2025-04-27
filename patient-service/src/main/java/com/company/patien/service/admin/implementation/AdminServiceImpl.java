c9ad731a5383969eb96389732af3d60e876b64ee
BloodSymph <rubakharoma@gmail.com>
2025-04-27 14:39:58 +0300

    Patient service v5!

--- patient-service/src/main/java/com/company/patien/service/admin/implementation/AdminServiceImpl.java
+++ patient-service/src/main/java/com/company/patien/service/admin/implementation/AdminServiceImpl.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.company.patien.mapper.admin.PatientAdminMapper.mapToPatientAdminDetailsResponse;
 
@@ -47,6 +48,7 @@ public class AdminServiceImpl implements AdminService {
     }
 
     @Override
+    @Transactional
     public void deletePatient(String username, Long version) {
 
     }
