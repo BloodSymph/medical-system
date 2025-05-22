7cbfb9a767a26d96792eaafbb7e5c68ad0ee42e0
BloodSymph <rubakharoma@gmail.com>
2025-05-22 18:25:43 +0300

    Patient service v7!

--- patient-service/src/main/java/com/company/patien/controller/InstrumentalController.java
+++ patient-service/src/main/java/com/company/patien/controller/InstrumentalController.java
@@ -1,9 +1,15 @@
 package com.company.patien.controller;
 
+import com.company.patien.dto.client.InstrumentalExaminationsRequest;
+import com.company.patien.dto.client.InstrumentalExaminationsResponse;
 import com.company.patien.service.client.InstrumentalExaminationsService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -12,7 +18,29 @@ public class InstrumentalController {
 
     private final InstrumentalExaminationsService instrumentalExaminationsService;
 
+    @GetMapping("/instrumental-examinations/{username}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<InstrumentalExaminationsResponse> getInstrumentalExaminations(
+            @PathVariable(value = "username") String username) {
+        return instrumentalExaminationsService.getInstrumentalExaminations(username);
+    }
 
+    @PostMapping("/instrumental-examinations/{username}/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public InstrumentalExaminationsResponse createInstrumentalExaminations(
+            @Valid @RequestBody InstrumentalExaminationsRequest instrumentalExaminationsRequest,
+            @PathVariable(value = "username") String username) {
+        return instrumentalExaminationsService.createInstrumentalExaminations(
+                instrumentalExaminationsRequest, username
+        );
+    }
 
+    @DeleteMapping("/instrumental-examinations/{username}/delete")
+    public ResponseEntity<String> deleteInstrumentalExaminationsResponse(
+            @PathVariable(value = "username") String username,
+            @RequestParam(value = "version", required = true) Long version) {
+        instrumentalExaminationsService.deleteInstrumentalExaminations(username, version);
+        return new ResponseEntity<>("Successful deleted", HttpStatus.OK);
+    }
 
 }
